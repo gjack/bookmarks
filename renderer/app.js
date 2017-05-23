f35392e26a82cc9f1f7754835689ca5d6bee9194
gjack <gabi@gabijack.com>
2017-05-22 20:41:50 -0400

    Add a modal to select url

---
+++ renderer/app.js
@@ -0,0 +1,9 @@
+// Show add modal
+$('.open-add-modal').click(() => {
+  $('#add-modal').addClass('is-active')
+})
+
+// Hide add modal
+$('.close-add-modal').click(() => {
+  $('#add-modal').removeClass('is-active')
+})
